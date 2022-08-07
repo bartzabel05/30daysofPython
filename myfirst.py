b6125ef1b7898f50c5045d29c025e3c6b3de231d
Kartik Kapoor <kartikk051101@gmail.com>
2022-08-07 16:38:23 +0530

    first commit

---
+++ myfirst.py
@@ -0,0 +1,10 @@
+profile={
+    'first_name':'Kartik',
+    'last_name':'Kapoor',
+    'age':20,
+    'skills':['HTML','CSS','JS','Python','C','C++'],
+    'is_married':False,
+    'country':'India',
+    'city':'New Delhi'
+}
+print(profile)
