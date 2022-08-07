fa94c2f841220f8256d26f2a1e305b3b9d601fdc
Kartik Kapoor <kartikk051101@gmail.com>
2022-08-07 17:03:18 +0530

    Day 1

---
+++ day1_helloworld.py
@@ -0,0 +1,19 @@
+#day1
+print(2+3) #add
+print(2-3) #subtract
+print(2*3) #multiply
+print(2**3) #exp
+print(4/2) #divide
+print(3%2) #mod
+print(3//2) #floor division
+
+print(type(1+3j))
+print(type([1,2,3]))
+print(type({1,2,3}))
+print(type((1,2,3)))
+print(type({'name':'kartik'}))
+
+#euclid distance sqrt(x2-x1)^2+sqrt(y2-y1)^2
+
+euclid_dist=((10-2)**2+((8-3)**2))**1/2
+print(euclid_dist)
